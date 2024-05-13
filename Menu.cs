7d3ba0e89c18a1c30f2bc2cbc2deb07e7f5b919c
Jesus Castro <castrojesus39@gmail.com>
2024-05-12 19:13:43 -0700

    created Menu

--- Menu.cs
+++ Menu.cs
@@ -2,5 +2,28 @@
 
 public class Menu
 {
-    
+    public void ShowMenu()
+    {
+        bool isOn = true;
+        do
+        {
+            Console.Write("What would you like to do?");
+            Console.WriteLine("Type 0 to Close Application");
+            Console.WriteLine("Type 1 to View All Records");
+            Console.WriteLine("Type 2 to Insert Record");
+            Console.WriteLine("Type 3 to Delete a Record");
+            Console.WriteLine("Type 4 to Update Record");
+            
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "0":
+                    isOn = false;
+                    break;
+            }
+            
+
+        } while (isOn);
+    }
 }
