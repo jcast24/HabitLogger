f58dcb4700ae8a62e90b49b557b535644c918993
Jesus Castro <castrojesus39@gmail.com>
2024-05-12 21:22:32 -0700

    changed menu stuff from both files

--- Menu.cs
+++ Menu.cs
@@ -7,13 +7,15 @@ public class Menu
         bool isOn = true;
         do
         {
-            Console.Write("What would you like to do?");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine();
             Console.WriteLine("Type 0 to Close Application");
             Console.WriteLine("Type 1 to View All Records");
             Console.WriteLine("Type 2 to Insert Record");
             Console.WriteLine("Type 3 to Delete a Record");
             Console.WriteLine("Type 4 to Update Record");
             
+            Console.Write("Your option: ");
             var option = Console.ReadLine();
 
             switch (option)
--- Program.cs
+++ Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear();
+        
+        // Start up message
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("Welcome to Habit Logger!");
+        Console.WriteLine("-------------------------");
+        Console.WriteLine();
+        
         // Call the menu
         Menu menu = new Menu();
         menu.ShowMenu();
