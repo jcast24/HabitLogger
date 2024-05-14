09dbbcdf68dcb535ef3050a248818baf081f2f8a
Jesus Castro <castrojesus39@gmail.com>
2024-05-13 21:26:16 -0700

    added connection to sql db via SQLite

--- Program.cs
+++ Program.cs
@@ -1,19 +1,41 @@
-﻿namespace HabitLogger;
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitLogger;
 
 class Program
 {
+    private static string connectionString = @"Data Source=habit.db";
     static void Main(string[] args)
     {
-        Console.Clear();
-        
-        // Start up message
-        Console.WriteLine("-------------------------");
-        Console.WriteLine("Welcome to Habit Logger!");
-        Console.WriteLine("-------------------------");
-        Console.WriteLine();
-        
-        // Call the menu
-        Menu menu = new Menu();
-        menu.ShowMenu();
+        // Console.Clear();
+        //
+        // // Start up message
+        // Console.WriteLine("-------------------------");
+        // Console.WriteLine("Welcome to Habit Logger!");
+        // Console.WriteLine("-------------------------");
+        // Console.WriteLine();
+        //
+        // // Call the menu
+        // Menu menu = new Menu();
+        // menu.ShowMenu();
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            // open the connection
+            connection.Open();
+            
+            // tell the connection to create a command
+            var tableCmd = connection.CreateCommand();
+
+            tableCmd.CommandText = "";
+
+            // ask the database not to return any values
+            tableCmd.ExecuteNonQuery();
+            
+            // close the connection with database
+            connection.Close();
+
+        }
+            
+
     }
 }
