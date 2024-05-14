fb50094e7ecc311120213cbfcad3216984fd887d
Jesus Castro <castrojesus39@gmail.com>
2024-05-14 15:49:24 -0700

    refractored menu, also added Engine.DisplayAll() to the first option in the menu

--- Menu.cs
+++ Menu.cs
@@ -10,10 +10,10 @@ public class Menu
             Console.WriteLine("What would you like to do?");
             Console.WriteLine();
             Console.WriteLine("Type 0 to Close Application");
-            Console.WriteLine("Type 1 to View All Records");
-            Console.WriteLine("Type 2 to Insert Record");
-            Console.WriteLine("Type 3 to Delete a Record");
-            Console.WriteLine("Type 4 to Update Record");
+            Console.WriteLine("Type 1 to Show all habits");
+            Console.WriteLine("Type 2 to Add a new habit");
+            Console.WriteLine("Type 3 to Delete a habit");
+            Console.WriteLine("Type 4 to Update a habit");
             
             Console.Write("Your option: ");
             var option = Console.ReadLine();
@@ -24,19 +24,22 @@ public class Menu
                     isOn = false;
                     break;
                 case "1":
+                    // Show all habits
+                    Engine.DisplayAll();
                     break;
                 case "2":
+                    // Add a new habit
                     break;
                 case "3":
+                    // Delete a habit
                     break;
                 case "4":
+                    // Update a habit
                     break;
                 default:
                     Console.WriteLine("Invalid Command. Please type a number from 0 to 4");
                     break;
             }
-            
-
         } while (isOn);
     }
 }
