986468670f17fd769839aad8ee8fd12368b7cde5
Jesus Castro <castrojesus39@gmail.com>
2024-05-14 15:51:02 -0700

    Engine.cs is going to run all our CRUD operations/methods

---
+++ Engine.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitLogger;
+
+public class Engine
+{
+    // private static string connectionString = @"Data source=habit.db";
+    public static void DisplayAll()
+    {
+        using (var connection = new SqliteConnection(Program.DbConnect))
+        {
+            connection.Open();
+
+            var tableCmd = connection.CreateCommand();
+
+            tableCmd.CommandText = @"SELECT * FROM habit";
+        }
+    }
+}
