c9c6c3b37e0d299bcba3299213002d834558b384
Jesus Castro <castrojesus39@gmail.com>
2024-05-24 17:05:00 -0700

    Program.cs had weird formatting that has been fixed

--- Program.cs
+++ Program.cs
@@ -16,27 +16,27 @@ class Program
         {
             // open the connection
             connection.Open();
-            
+
             // tell the connection to create a command
             var tableCmd = connection.CreateCommand();
-            
+
             // create the table if it doesn't exist
-            tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS habit (ID INTEGER PRIMARY KEY AUTOINCREMENT, habitName TEXT, Date TEXT)";
+            tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS habit (Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Date TEXT, Hours INTEGER)";
 
             // ask the database not to return any values
             tableCmd.ExecuteNonQuery();
-            
+
             // close the connection with database
             connection.Close();
         }
-        
+
         Console.Clear();
         // Start up message
         Console.WriteLine("-------------------------");
         Console.WriteLine("Welcome to Habit Logger!");
         Console.WriteLine("-------------------------");
         Console.WriteLine();
-                
+
         // Call the menu
         Menu menu = new Menu();
         menu.ShowMenu();
