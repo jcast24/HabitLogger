bcd3a5c2393036904e795c3ac4a563f574f1d2db
Jesus Castro <castrojesus39@gmail.com>
2024-05-24 17:04:24 -0700

    finished DisplayAll() method to display all the contents from the db

--- Engine.cs
+++ Engine.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using System.Data;
+using System.Globalization;
+using Microsoft.Data.Sqlite;
 
 namespace HabitLogger;
 
@@ -14,6 +16,58 @@ public class Engine
             var tableCmd = connection.CreateCommand();
 
             tableCmd.CommandText = @"SELECT * FROM habit";
+
+            List<Habit> tableData = [];
+
+            SqliteDataReader reader = tableCmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    tableData.Add(new Habit
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Date = DateTime.ParseExact(reader.GetString(2), "dd-MM-yyyy", new CultureInfo("en-US")),
+                        NumberOfHours = reader.GetInt32(3)
+                    });
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found");
+            }
+            
+            connection.Close();
+            
+            Console.WriteLine();
+            foreach (var hb in tableData)
+            {
+                Console.WriteLine($"{hb.Id} {hb.Name} {hb.Date} {hb.NumberOfHours}");
+            }
+            Console.WriteLine();
+        }
+    }
+    public static void Insert()
+    {
+        Console.WriteLine("Enter name of habit: ");
+        string name = Console.ReadLine();
+            
+        Console.WriteLine("Enter date in format dd-MM-yyyy: ");
+        string date = Console.ReadLine();
+        
+        Console.WriteLine("Enter the number of hours: ");
+        int numOfHours = int.Parse(Console.ReadLine());
+            
+        using (var connection = new SqliteConnection(Program.DbConnect))
+        {
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+            tableCmd.CommandText = $"INSERT INTO Habit(Name, Date, NumberOfHours) VALUES('{name}, {date} ,{numOfHours}')";
+            tableCmd.ExecuteNonQuery();
+            
+            connection.Close();
         }
     }
 }
