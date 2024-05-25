c6502d600fbedc47424dc0a2d21fddde153b92ff
Jesus Castro <castrojesus39@gmail.com>
2024-05-24 17:04:37 -0700

    fixed minor error with Habit class

--- Habit.cs
+++ Habit.cs
@@ -9,9 +9,7 @@ namespace HabitLogger
     {
         public string Name { get; set; } = string.Empty;
         public int Id { get; set; } = 0;
-
         public DateTime Date { get; set; }
-
         public int NumberOfHours { get; set; } = 0;
     }
 }
